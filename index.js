22be37f3d88cc8c181b2b85d3e425b60d2b27106
Paul Thomas <paulthomas2107@gmail.com>
2021-07-08 19:10:39 +0100

    More

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.listen(port, () => {
   console.log(`App Started: Listening on port ${port}`);
 });
 
+// Get Dino Name
 app.get("/dinoname", async (request, response) => {
   const fetchApi = await fetch(
     "https://alexnormand-dino-ipsum.p.rapidapi.com/?paragraphs=1&words=2&format=json",
@@ -22,6 +23,22 @@ app.get("/dinoname", async (request, response) => {
   );
 
   const dinoNameResponse = await fetchApi.json();
-  console.log(dinoNameResponse);
   response.json(dinoNameResponse);
+});
+
+// Get Dino Image
+app.get("/dinoimage", async (request, response) => {
+  const fetchApi = await fetch(
+    "https://bing-image-search1.p.rapidapi.com/images/search?q=dinosaur&count=5",
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "e7b118db4bmshe2c07f49b82bd4fp1870dcjsn2edda42bd3aa",
+        "x-rapidapi-host": "bing-image-search1.p.rapidapi.com",
+      },
+    }
+  );
+
+  const dinoImageResponse = await fetchApi.json();
+  response.json(dinoImageResponse);
 });
